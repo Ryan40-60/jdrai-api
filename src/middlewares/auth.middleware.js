245220d6bc2b67d67ed833ddf75fc5f4494ad830
Ryan DEPREZ <ryan@40-60studio.com>
2023-07-27 21:22:31 +0200

    role verification added in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,12 +3,15 @@ import passport from "passport";
 
 import ApiError from "../class/ApiError.js";
 
+import { roles } from "../config/enum.config.js";
+
 /**
  * @description: Checks if the user is authenticated
  *
+ * @params {boolean} needAdmin - check if the user is admin if true
  * @returns A middleware that authenticates the user
  */
-const auth = () => {
+const auth = (needAdmin) => {
   return async (req, res, next) => {
     // Create a promise to handle the success or failure of authentication
     new Promise((resolve, reject) => {
@@ -24,6 +27,12 @@ const auth = () => {
 
         // Add the user to the request
         req.user = user;
+
+        // Check if the user is an admin if necessary
+        if (needAdmin && user.role !== roles.ADMIN) {
+          // Reject the promise with a 403 error
+          return reject(new ApiError(httpStatus.FORBIDDEN, "Unauthorized"));
+        }
 
         // Finally, resolve the promise
         resolve();
